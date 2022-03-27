e0b56c756393ac10e2356a97ac0a45ef0f6e9ac1
AiristEsp <eairist@gmail.com>
2022-03-27 21:04:42 +0700

    3.1 Project

---
+++ 3.TreasureIsland/3.1_OddOrEven.py
@@ -0,0 +1,9 @@
+
+# Odd or Even
+
+number = int(input("Which number do you want to check? "))
+
+if number % 2 == 0:
+    print(f"This is {number} an odd number ")
+else:
+    print("This is {number} an even number")
